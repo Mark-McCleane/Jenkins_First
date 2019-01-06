a4e454726aa6f1aad874bdcb93f791765d04559e
Mark McCleane <mccleanemark177@gmail.com>
2019-01-06 21:48:57 +0000

    fixed errors

--- Student.java
+++ Student.java
@@ -10,10 +10,11 @@ public class Student {
 
     public AttendanceGrade getAttendanceGrade(int attendance) {
         if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
+        if (attendance == 0) { return AttendanceGrade.ABSENT;}
         if (attendance < 30) return AttendanceGrade.VERY_POOR;
-        if (attendance < 50) return AttendanceGrade.POOR;
+//        if (attendance < 50) return AttendanceGrade.POOR; doesn't say on brief about < 50
         if (attendance < 70) return AttendanceGrade.AVERAGE;
-        if (attendance <= 90) return AttendanceGrade.GOOD;
+        if (attendance < 90) return AttendanceGrade.GOOD;
         return AttendanceGrade.VERY_GOOD;
     }
 }
--- studentTest.java
+++ studentTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 
+
 /**
  * Created by meudecc on 15/02/2017.
  */
